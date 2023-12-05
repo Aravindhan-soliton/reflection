0ec4084e0fbc530af489785f6be170128fc08757
Aravindhan Palanisamy <aravindhan.palanisamy@solitontech.local>
2023-12-05 14:52:08 +0530

    add property update

---
+++ ReflectionPropertyConsoleApp/Models/Sample.cs
@@ -0,0 +1,7 @@
+public class Sample
+{   
+    public string File { get; set; } = "File";
+    public string Name { get; set; } = "Name";
+    public string Content { get; set; } = "Content";
+    public string Driver { get; set; } = "Driver";
+}
---
+++ ReflectionPropertyConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+System.Console.WriteLine("Enter the property name who's value is to be changed: ");
+string? inputPropertyName = Console.ReadLine(); 
+
+System.Console.WriteLine("Enter the value: ");
+string? inputPropertyValue = Console.ReadLine();
+
+Sample sampleObject = new Sample();
+
+Type classType = typeof(Sample);
+PropertyInfo propertyInfo = classType.GetProperty(inputPropertyName);
+PropertyInfo[] properties = classType.GetProperties();
+
+if (propertyInfo != null){
+     propertyInfo.SetValue(sampleObject, inputPropertyValue, null);
+}
+else{
+    Console.WriteLine($"Property '{inputPropertyName}' does not exist in MyClass.");
+}
+
+
+foreach(PropertyInfo property in properties){
+    System.Console.WriteLine($"value of {property.Name} : {property.GetValue(sampleObject)}");
+}
+
